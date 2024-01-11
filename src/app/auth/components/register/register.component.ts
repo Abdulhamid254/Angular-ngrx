ce55f7fa27271143e96ffd0abc31a4edbab7ab06
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2024-01-11 01:50:27 -1100

    Installed redux devtools and worked implemented auth store

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,17 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { register } from '../../store/actions';
+import { RegisterRequestInterface } from '../../types/registerRequest.interface';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule],
+  imports: [CommonModule,ReactiveFormsModule,RouterModule,],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent implements OnInit{
   reactiveForm!: FormGroup;
 
+
+  constructor(private store: Store){}
 
   ngOnInit() {
     this.reactiveForm = new FormGroup({
@@ -23,7 +29,11 @@ export class RegisterComponent implements OnInit{
 
 
   onSubmit() {
-    console.log(this.reactiveForm);
+    console.log(this.reactiveForm.getRawValue())
 
+    const request: RegisterRequestInterface = {
+      user:this.reactiveForm.getRawValue()
+    }
+    this.store.dispatch(register({ request }));
   }
 }
